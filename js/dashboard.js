74272e60781034bb9393f9ed54464c574763346a
Himanshu Kamboj <himanshukamboj0027@gmail.com>
2026-07-10 13:11:19 +0530

    dashboard page almost complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,3 +5,29 @@ logoutBtn.addEventListener("click", () => {
     localStorage.removeItem("loggedIn");
     window.location.href = "index.html";
 });
+
+
+function updateTimeDate() {
+    const now = new Date();
+
+    const date = now.toLocaleDateString("en-IN", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
+    const time = now.toLocaleTimeString("en-IN", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    });
+
+    document.getElementById("current-date").textContent = date;
+    document.getElementById("current-time").textContent = time;
+}
+
+updateTimeDate();
+
+setInterval(updateTimeDate, 1000);
